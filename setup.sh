b042c730da3db10874e8814b7c2db6fde731cee2
SteliosMalamas <malamas_paros02@yahoo.com>
2022-07-26 17:48:14 +0300

    Added setup.sh for automatic installation and setup. Updated README.md

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -x "$(command -v apt-get)" ]; then
+	printf "apt package manager detected.\nInstalling packages\n"
+	sudo apt-get install gcc make libncurses5-dev libncursesw5-dev -y
+elif [ -x "$(command -v pacman)" ]; then
+	printf "pacman package manager detected.\nInstalling packages\n"
+	sudo pacman -S gcc make ncurses
+else
+	printf "Package manager not found. Read README.md for installation instructions\n">&2
+	exit 1; fi
+
+sudo make install
+cp bin/typo .
+printf "\nInstallation Complete!\nUsage: typo <filename>\n"
+exit 0
